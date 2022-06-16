a98d59f72db088365365cb34eea2cde8ea66412a
20182368@office.daejin.ac.kr <20182368@office.daejin.ac.kr>
2022-06-16 22:19:55 +0900

    API 세팅 시작

---
+++ Pos 4.0/Pos 4.0/API.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pos_4._0
+{
+    internal class API
+    {
+
+
+
+    }
+}
