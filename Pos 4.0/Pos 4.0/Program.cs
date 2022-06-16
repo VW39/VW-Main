2a0bde6a0a08164fec31882d04d3a562958367b4
cyy3492r@o.shinhan.ac.kr <cyy3492r@o.shinhan.ac.kr>
2022-06-16 22:12:43 +0900

    수정을 안누르고 커밋 그전에 우측 하단에 스테이징 +

--- Pos 4.0/Pos 4.0/Program.cs
+++ Pos 4.0/Pos 4.0/Program.cs
@@ -12,12 +12,6 @@ namespace Pos_4._0
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
-        }//앞으로 이렇게 쓰면됨 하단 스마트 포스 2.0은 그냥 테스트 고
-        //pos4.0 계속 쓸꺼임 ㅇㅋ??
-<<<<<<< HEAD
-        //ㅎ g2 내가해봄
-=======
-        //ㅎ g2
->>>>>>> 491747795e7143e51626661b8ad383ba6bd779b3
+        } 
     }
 }
